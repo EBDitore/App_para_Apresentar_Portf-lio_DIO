a325e00b81f15ea96e1f0031d15f880a8f0d908c
Elielson <ebditore@gmail.com>
2022-08-23 20:33:04 -0300

    Módulo App + Koin adicionados

--- app/src/main/java/meu/teste/portfoliopresentation/ui/MainActivity.kt
+++ app/src/main/java/meu/teste/portfoliopresentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package meu.teste.portfoliopresentation
+package meu.teste.portfoliopresentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import meu.teste.portfoliopresentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
